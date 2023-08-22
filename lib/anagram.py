3b8e650b1c148746a517de8130d01e46a4868888
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-08-22 15:19:34 +0300

    Finished anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        anagrams = []
+        sorted_word = sorted(self.word.lower())  # Convert to lowercase for case-insensitive comparison
+
+        for candidate in word_list:
+            candidate_lower = candidate.lower()
+            if candidate_lower != self.word.lower() and sorted(candidate_lower) == sorted_word:
+                anagrams.append(candidate)
+
+        return anagrams
